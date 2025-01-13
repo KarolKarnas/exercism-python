5459164c707e78e5fd55eb4006244131d4a253c2
Karol Karnas <karol.karnas@gmail.com>
2025-01-13 21:05:09 +0100

    fix tests in resistor color expert

--- resistor-color-expert/resistor_color_expert.py
+++ resistor-color-expert/resistor_color_expert.py
@@ -1,14 +1,14 @@
 COLORS = [
-    "black",
-    "brown",
-    "red",
-    "orange",
-    "yellow",
-    "green",
-    "blue",
-    "violet",
-    "grey",
-    "white",
+    'black',
+    'brown',
+    'red',
+    'orange',
+    'yellow',
+    'green',
+    'blue',
+    'violet',
+    'grey',
+    'white'
 ]
 
 TOLERANCE_MAP = {
@@ -19,34 +19,43 @@ TOLERANCE_MAP = {
     'brown': 1,
     'red': 2,
     'gold': 5,
-    'silver': 10,
+    'silver': 10
 }
 
-PREFIX_MAP = {'giga': 1000000000, 'mega': 1000000, 'kilo': 1000}
-
+PREFIX_MAP = {
+    'giga': 1000000000,
+    'mega': 1000000,
+    'kilo': 1000
+}
 
 def resistor_label(colors):
+    if len(colors) not in [4, 5]:
+        return '0 ohms'
+        
     tolerance = ''
     prefix = ''
     ohms = 0
+    
     if len(colors) == 4:
         ohms = (
-            COLORS.index(colors[0]) * 10 + COLORS.index(colors[1])
+            COLORS.index(colors[0]) * 10 + 
+            COLORS.index(colors[1])
         ) * 10 ** COLORS.index(colors[2])
         tolerance = f' ±{TOLERANCE_MAP[colors[-1]]}%'
+    
     if len(colors) == 5:
         ohms = (
-            COLORS.index(colors[0]) * 100
-            + COLORS.index(colors[1]) * 10
-            + COLORS.index(colors[2])
+            COLORS.index(colors[0]) * 100 +
+            COLORS.index(colors[1]) * 10 +
+            COLORS.index(colors[2])
         ) * 10 ** COLORS.index(colors[3])
         tolerance = f' ±{TOLERANCE_MAP[colors[-1]]}%'
-
+    
     for key, value in PREFIX_MAP.items():
         if ohms >= value:
             prefix = key
             ohms = ohms / value
-            
-    ohms = int(ohms) if ohms.is_integer() else round(ohms, 2)
+    
+    ohms = int(ohms) if ohms == int(ohms) else round(ohms, 2)
     
     return f'{ohms} {prefix}ohms{tolerance}'
