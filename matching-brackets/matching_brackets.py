3cd542c0db3420d1b8b5142b6f7347b5dfab7076
Karol Karnas <karol.karnas@gmail.com>
2025-03-23 16:31:58 +0100

    mTCHING BRACKETS

---
+++ matching-brackets/matching_brackets.py
@@ -0,0 +1,20 @@
+OPEN = ['[', '{', '(']
+CLOSE = [']', '}', ')']
+
+
+def is_paired(input_string):
+    stack = []
+    
+    for char in input_string:
+        if char in OPEN:
+            open_index = OPEN.index(char)
+            stack.append(CLOSE[open_index])
+            print(1, stack)
+        elif char in CLOSE:
+            if len(stack) == 0 or stack.pop()!= char:
+                return False 
+            print(2, stack)
+    return len(stack) == 0
+
+
+print(is_paired("([{}({}[])])"))
